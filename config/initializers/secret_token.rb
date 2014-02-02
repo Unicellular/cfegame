fab655bbdf4a86529870b4766471f30233682721
Rex <rexkimta@gmail.com>
2014-02-02 18:12:43 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cfegame::Application.config.secret_key_base = 'd334e9cd101351447669fdf7ec6849b755c921feb5aff0e5fd493bf9ecdbca3b1cb06e198bf37eb1a3fc33ccbff4cf5ad84c9fdd2bd868f3d9d28c87d37e06ef'
