c9454cf74a7015ebac439fee8256d07198a1781f
Perry Chou <906135+mbcoguno@users.noreply.github.com>
2022-08-29 16:12:06 +0800

    Move jquery to importmap. Also upgrade to latest.
    * Use jQuery 3.

--- config/importmap.rb
+++ config/importmap.rb
@@ -2,3 +2,5 @@
 
 pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
+pin "jquery", to: "https://code.jquery.com/jquery-3.6.1.min.js", preload: true
+pin "jquery-ujs", to: "https://ga.jspm.io/npm:jquery-ujs@1.2.3/src/rails.js", preload: true
