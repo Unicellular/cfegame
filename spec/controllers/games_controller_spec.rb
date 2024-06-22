7707d3bc96479fd83cb7614ee80eec49f3661411
rex <rexkimta@gmail.com>
2024-06-22 10:26:54 +0800

    增加controller端的測試

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -9,4 +9,16 @@ RSpec.describe GamesController, type: :controller do
       expect(response).to have_http_status(:success)
     end
   end
+
+  describe "GET event_list" do
+    it "forbids with wrong player" do
+      @user1 = User.create
+      @user2 = User.create
+      @game = Game.open(@user1)
+      @game.join_with(@user2, @game.teams[1])
+      @player2 = @game.players.find_by(user: @user2)
+      get :event_list, params: { id: @game.id, player_id: @player2.id }
+      expect(response).to have_http_status(:forbidden)
+    end
+  end
 end
